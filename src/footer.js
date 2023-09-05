a815f743e50cca9e7b8459fc290dd50a4fc0cdf5
Varun Kumar Reddy Pothula <117117037+Bunny2906@users.noreply.github.com>
2023-09-05 12:16:33 +0530

    Update footer.js

--- src/footer.js
+++ src/footer.js
@@ -1,9 +1,8 @@
 import React from 'react'
-
 const footer = () => {
   return (
     <div className='footer'>
-        <h5>&copy;Copyright LMS App 2023</h5>
+        <h5>&copy;Copyright e-Book App 2023</h5>
     </div>
   )
 }
