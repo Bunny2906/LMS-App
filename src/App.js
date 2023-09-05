00251f4224ef0a611be9b9f2d7a8e50eac897af3
Varun Kumar Reddy Pothula <117117037+Bunny2906@users.noreply.github.com>
2023-09-05 12:15:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,42 @@ import {auth} from './firebase';
 import './App.css';
 import './logins.css';
 import Home from './home';
+import Home2 from './home2';
 import Navbar from './navbar';
-import Signin from './pages/signin'
-import Signup from './pages/signup'
-import Admin from './pages/admin'
-import Footer from './footer'
+import Navbar2 from './navbar2';
+import Signin from './pages/signin';
+import Signup from './pages/signup';
+import Admin from './pages/admin';
+import BookList from './booklist';
+import Favourites from './favourites';
+import Footer from './footer';
 import {Route,Routes,BrowserRouter,Switch} from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect, useState,useContext,Navigate} from 'react';
 function App() {
- 
+  const [userName,setUserName] = useState("");
+    useEffect(()=>{
+        auth.onAuthStateChanged((user)=>{
+            if(user)
+            {
+                setUserName(user.displayName);
+            }
+            else
+            {
+                setUserName("");
+            }
+        });
+    },[])
   return (
     <div className="App">
-        <Navbar />
+          {auth?.currentUser?(<Navbar />):(<Navbar2 />)}
           <Routes>
-            <Route exact path='/home' element={<Home />}></Route>
-            <Route exact path='/signin' element={<Signin></Signin>}></Route>
+            <Route exact path='/' element={<Home2 />}></Route>
+            <Route exact path='/home' element={<Home name={userName}/>}></Route>
+            <Route exact path={'/signin'} element={<Signin></Signin>}></Route>
             <Route exact path="/signup" element={<Signup />}></Route>
             <Route exact path="/admin" element={<Admin />}></Route>
+            <Route exact path="/booklist" element={<BookList />}></Route>
+            <Route exact path="/favourites" element={<Favourites />}></Route>
           </Routes>  
     </div>
   );
