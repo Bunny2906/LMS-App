f9674136541f77bdac09f283a5ae5af36e4a3aa2
Varun Kumar Reddy Pothula <117117037+Bunny2906@users.noreply.github.com>
2023-09-08 14:48:54 +0530

    Add files via upload

--- src/bookdetails.js
+++ src/bookdetails.js
@@ -1,14 +1,31 @@
 import React,{useState,useEffect} from "react";
+import { useNavigate,useParams} from "react-router-dom";
+import './books.css';
 import axios from 'axios';
-function BookDetails(book_id)
+function BookDetails()
 {
-    const URL = `https://openlibrary.org/works/${book_id}.json`;
+    const {id} = useParams();
+    const [description,setDescription] = useState("");
+    const [title,setTitle] = useState("");
+    const [coverImg,setCoverImg] = useState("");
+    const URL = `https://openlibrary.org/works/`;
+    let response;
     useEffect(()=>{
-        axios.get(URL).then((response)=>{console.log(response.data)})
-        .catch((err)=>{console.log(err)})
-    })
+        function getBookDetails()
+        {
+            axios.get(`${URL}${id}.json`)
+            .then((res)=>{setCoverImg(`https://covers.openlibrary.org/b/id/${res.data.covers[0]}-M.jpg`);
+            console.log(res.data);setTitle(res.data.title);setDescription(res.data.description ? (res.data.description):("no description found"))})
+            .catch((err)=>{console.log(err)});
+        }
+        getBookDetails();
+    },[id])
     return <div>
-
-            </div>
+                <div className="bookdetails">
+                    <h2 className="title">{title}</h2>
+                    <img src={coverImg} alt={title}></img>
+                    <p className="description"><u><h3>Description :</h3></u>{description}</p>
+                </div>
+           </div>
 }
 export default BookDetails;
